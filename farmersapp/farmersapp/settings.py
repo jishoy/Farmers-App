393e8763eab7341e77255cdf2c0cb378fa49c18a
Goutham Namboodiri <gouthamm>
2021-01-25 17:18:38 +0530

    small fixes

--- farmersapp/farmersapp/settings.py
+++ farmersapp/farmersapp/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'farmersapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'farmer_db_current',
+        'NAME': 'farmer_db',
         'USER': 'user',
         'PASSWORD': '',
         'HOST': '',   # Or an IP Address that your DB is hosted on
